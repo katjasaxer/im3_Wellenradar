01b853cdbf7264e264466bc1c04a5017824c5bde
Katja <katja.saxer@stud.fhgr.ch>
2025-10-09 15:17:55 +0200

    dies und das

--- script.js
+++ script.js
@@ -21,19 +21,20 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("Abgerufene Daten:", data);
 
       const ctx = document.getElementById("wellenChart").getContext("2d");
+
       const datasets = Object.keys(data).map((boje) => ({
-        label: city,
+        label: boje,
         data: data[boje].map((item) => item.wellen_höhe),
         fill: false,
         borderColor: getRandomColor(), // Generiert eine zufällige Farbe für jede Stadtlinie im Diagramm
         tension: 0.1, // Gibt der Linie im Diagramm eine leichte Kurve
       }));
 
-      /* Uncomment to create the chart
+      Uncomment to create the chart
       new Chart(ctx, {
         type: "line",
         data: {
-          labels: data["Bern"].map((item) => new Date(item.created_at).toLocaleDateString()), // Nimmt an, dass alle Städte Daten für dieselben Daten haben
+          labels: data["Albion"].map((item) => new Date(item.created_at).toLocaleDateString()), // Nimmt an, dass alle Städte Daten für dieselben Daten haben
           datasets: datasets,
         },
         options: {
@@ -49,13 +50,13 @@ document.addEventListener("DOMContentLoaded", () => {
     .catch((error) => console.error("Fetch-Fehler:", error)); // Gibt Fehler im Konsolenlog aus, falls die Daten nicht abgerufen werden können
 
   function getCityColor(city) {
-    const cityColors = {
-      Bern: "#ffcf33ff",
-      Zürich: "#33a3ffff",
-      Chur: "#2edc07ff",
+    const wellenFarben = {
+      swht: "#09008aff",
+      wwh: "#97e567ff",
+  
       // Fügen Sie hier weitere Städte und ihre Farben hinzu
     };
-    return cityColors[city] || getRandomColor(); // Gibt die vordefinierte Farbe zurück oder eine zufällige Farbe
+    return wellenFarben[boje] || getRandomColor(); // Gibt die vordefinierte Farbe zurück oder eine zufällige Farbe
   }
 
   function getRandomColor() {
